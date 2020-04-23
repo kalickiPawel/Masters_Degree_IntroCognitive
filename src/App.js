b9b6a0fd235132c35c51a57b4d31d6ed3d573816
Jakub Piskorowski <jakub9616@gmail.com>
2020-04-23 17:23:34 +0200

    Images

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import Header from './header';
 import Image from './image';
 import Question from './question';
 
+
+
 const App = () => {
   const [seconds, setSeconds] = useState(0);
-  let tasks = ["Tresc 1", "Tresc 2"];
-  let task;
-  if (seconds < 5) {
-    task = tasks[0];
+  let task, image;
+
+  for (let i = 0; i < 75; i++) {
+    
+
+    let tasks = ["Tresc 1", "Tresc 2"];
+    
+    if (seconds < 5) {
+
+      //Display image
+      //var img = document.createElement("img");
+      //img.src = './koperty/BlackBlue.png';
+      //image = img;
+    }
+    else {
+      task = tasks[1];
+      //sprawdza czy przycisk zostal wcisniety i ustawia czas na 0
+      setSeconds(0);
+    }
   }
-  else {
-    task = tasks[1];
-  }
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,12 +38,16 @@ const App = () => {
     return () => clearInterval(interval);
   }, []);
 
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Header task={task} />
         {seconds} seconds have elapsed since mounting.
-        <Image name="Pawel" />
+        <Image name={image} />
         <Question name="Jakub" />
       </header>
     </div>
