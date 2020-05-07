e9146a2cbd2521522ef5a41f58f41de307935547
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-07 16:03:19 +0200

    Added images array

--- src/image/Image.js
+++ src/image/Image.js
@@ -1,12 +1,47 @@
 import React from 'react';
 
 
+class Image extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            images: this.importAll(require.context('../koperty/', false, /\.(png|jpe?g|svg)$/))
+        };
+        // let items = [{ "key": "r", "image": require("./img/image1.jpg") },
+        // { "key": "b", "image": require("./img/image2.jpg") },
+        // { "key": "j", "image": require("./img/image3.jpg") }
+        // ];
+        // this.state = { image: loadImage() }; //instead of loadImage(0);
+    }
 
-function Image(props) {
-    return (
-        <div className="image">
+    importAll(r) {
+        return r.keys().map(r);
+    }
 
-        </div>
-    )
+    // loadImage(counter) {
+    //     //initialize the counter to 0 if no counter was passed
+    //     counter = counter || 0;
+    //     var i = document.getElementById("img" + counter);
+    //     if (counter == imgAddresses.length) { return; }
+    //     i.onload = function () {
+    //         loadImage(counter + 1)
+    //     };
+    //     i.src = imgAddresses[counter];
+    // }
+
+    componentDidMount() {
+        console.log(this.state.images);
+    }
+
+    render() {
+        return (
+            <div className="image" >
+                {this.state.images.map((image, index) => {
+                    return <li key={index}><img src={image} /></li>
+                })}
+                Hello world!
+            </div>
+        );
+    }
 }
 export default Image;
