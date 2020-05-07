bfc3ecf43101e8d63c2d47a70613e9e8d05666cb
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-07 16:02:58 +0200

    Added questions answers

--- src/question/Question.js
+++ src/question/Question.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+class Question extends React.Component {
+    constructor(props) {
+        super();
+        this.state = { answer: ["wspaniały", "dobry", "przeciętny", "słaby", "zły"] };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e, value) {
+        // console.log(e);
+        const timestamp = Date.now();
+        console.log(new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(timestamp));
+        console.log(value.index + 1);
+
+    }
 
 
 
-function Question(props) {
-    return (
-        <div className="question">
-
-        </div>
-    )
+    render() {
+        return (
+            <div className="question">
+                {this.state.answer.map((value, index) => {
+                    return <Button className=".ml-1" value={index} variant="primary" size="lg" onClick={e => this.handleClick(e, { index })}>{value}</Button>
+                })
+                }
+            </div >
+        );
+    }
 }
 export default Question;
