221fd80dd7a112ec9804515197dabbb6981be6a1
Paweł Kalicki <kalicki.paul@icloud.com>
2020-05-18 12:34:59 +0200

    Added read random one image

--- src/image/Image.js
+++ src/image/Image.js
@@ -7,39 +7,23 @@ class Image extends React.Component {
         this.state = {
             images: this.importAll(require.context('../koperty/', false, /\.(png|jpe?g|svg)$/))
         };
-        // let items = [{ "key": "r", "image": require("./img/image1.jpg") },
-        // { "key": "b", "image": require("./img/image2.jpg") },
-        // { "key": "j", "image": require("./img/image3.jpg") }
-        // ];
-        // this.state = { image: loadImage() }; //instead of loadImage(0);
     }
 
     importAll(r) {
         return r.keys().map(r);
     }
 
-    // loadImage(counter) {
-    //     //initialize the counter to 0 if no counter was passed
-    //     counter = counter || 0;
-    //     var i = document.getElementById("img" + counter);
-    //     if (counter == imgAddresses.length) { return; }
-    //     i.onload = function () {
-    //         loadImage(counter + 1)
-    //     };
-    //     i.src = imgAddresses[counter];
-    // }
-
     componentDidMount() {
         console.log(this.state.images);
     }
 
     render() {
+        const min = 0;
+        const max = this.state.images.length;
+        const rand = Math.floor(min + Math.random() * (max - min));
         return (
             <div className="image" >
-                {this.state.images.map((image, index) => {
-                    return <li key={index}><img src={image} /></li>
-                })}
-                Hello world!
+                <img src={this.state.images[rand]} />
             </div>
         );
     }
