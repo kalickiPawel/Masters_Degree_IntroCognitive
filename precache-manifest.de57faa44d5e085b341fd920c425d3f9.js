4c1ff8fea7889b3c7b3919e0e2c995936bdc5c79
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 13:20:33 +0000

    Deploy kalickiPawel/Masters_Degree_IntroCognitive to github.com/kalickiPawel/Masters_Degree_IntroCognitive.git:gh-pages

---
+++ precache-manifest.de57faa44d5e085b341fd920c425d3f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e828522a7e1e0a26bdca3468ac1e7fc",
+    "url": "/Masters_Degree_IntroCognitive/index.html"
+  },
+  {
+    "revision": "93506124a7441c85839f",
+    "url": "/Masters_Degree_IntroCognitive/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e93e28bf5fbdf7c3735f",
+    "url": "/Masters_Degree_IntroCognitive/static/css/main.de2d8d4d.chunk.css"
+  },
+  {
+    "revision": "93506124a7441c85839f",
+    "url": "/Masters_Degree_IntroCognitive/static/js/2.d0f41b3f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Masters_Degree_IntroCognitive/static/js/2.d0f41b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93e28bf5fbdf7c3735f",
+    "url": "/Masters_Degree_IntroCognitive/static/js/main.2d4a11f4.chunk.js"
+  },
+  {
+    "revision": "575726b125d120214876",
+    "url": "/Masters_Degree_IntroCognitive/static/js/runtime-main.82276784.js"
+  }
+]);
